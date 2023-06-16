e6a6a652a16e9103e973fc36651a095cb0689f21
hebaehab2001 <hebaehab74@gmail.com>
2023-06-16 06:15:57 -0700

    done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,86 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
- * find -> Finds the biggest number
- * @a: array
- * @n: Array Size
- * Return: biggest number
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
  */
-
-int find(int *a, int n)
-{
-	int i, big;
-
-	big = a[0];
-	for (i = 1; i < n; i++)
-	{
-		if (a[i] > big)
-			big = a[i];
-	}
-	return (big);
-}
-
-/**
- * mult_char -> Multiplies each char of username
- * @usr: Username
- * @len: Username length
- * Return: Multiplied username
- */
-
-int mult_char(char *usr, int len)
-{
-	int i, mult;
-
-	mult = 1;
-	for (i = 0; i < len; i++)
-		mult *= usr[i];
-	return (mult);
-}
-
-/**
- * gen_pass -> Generates a random char
- * @usr: Username
- * Return: Random char
- */
-
-int gen_pass(char *usr)
-{
-	int i, len, mult, big, seed, key;
-	int arr[4];
-
-	len = 0;
-	while (usr[len] != '\0')
-		len++;
-	srand(time(NULL));
-	seed = rand();
-	srand(seed);
-	for (i = 0; i < 4; i++)
-		arr[i] = rand();
-	mult = mult_char(usr, len);
-	big = find(arr, 4);
-	key = (mult ^ big);
-	return (key);
-}
-
-/**
- * main -> Entry point
- * @argc: Number of arguments
- * @argv: Arguments
- * Return: 0 on success, 1 on failure
- */
-
 int main(int argc, char *argv[])
 {
-	int key;
+	unsigned int i, b;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "      ";
 
 	if (argc != 2)
 	{
-		printf("Usage: %s username\n", argv[0]);
+		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-	key = gen_pass(argv[1]);
-	printf("%d\n", key);
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= argv[1][i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+		if ((char)b <= argv[1][i])
+			b = argv[1][i];
+	srand(b ^ 14);
+	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
+	printf("%s\n", p);
 	return (0);
 }
