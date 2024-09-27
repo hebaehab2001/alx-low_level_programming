5ff7c936878f48666538853ce992a77adf2acc39
Heba Ehab <95038107+hebaehab2001@users.noreply.github.com>
2024-09-27 13:23:15 +0300

    Create search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
